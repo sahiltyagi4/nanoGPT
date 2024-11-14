05b1229b9493b8400c623758fbe1f1f96f8a4150
sahiltyagi4 <sahilt.tyagi@gmail.com>
2024-11-14 11:40:56 -0500

    running baseline

--- exec_script/gpt_baseline.sh
+++ exec_script/gpt_baseline.sh
@@ -3,6 +3,7 @@
 # install tiktoken and hugging face datasets
 cd /workspace/nonoGPT
 
+outdir='/workspace/output/'
 dir='/workspace/output/'
 interface='eth0'
 addr='172.17.0.2'
@@ -15,4 +16,4 @@ container='worker'$rank
 echo '###### going to launch training for rank '$procrank ' on container '$container' with bsz '$batchsize
 
 python3 -m run_nanoGPT --dir=$dir --interface=$interface --batch-size=$batchsize --world-size=$worldsize \
---master-addr=$addr --rank=$procrank &
+--master-addr=$addr --rank=$procrank --out-dir=$outdir &
